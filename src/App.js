e34704ade0d79269b4957a5a3b0a3fa688a64056
sushantkdm <sushantkadam936@gmail.com>
2023-06-17 20:55:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import SignUp from './SignUp';
+
+function App() {
+  return (
+    <div className="App">
+     <p>Hello</p>
+     <SignUp></SignUp>
+    </div>
+  );
+}
+
+export default App;
